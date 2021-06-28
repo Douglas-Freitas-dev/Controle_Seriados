16b3bedf521bf86a4dc41c919564688204e2ef8d
Douglas Freitas <freitasd345@gmail.com>
2021-06-28 18:13:50 -0300

    Second Commit - Implementação das pages e estruturação das rotas

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', function(){
-    $series = [
-        'Grey\'s Anatomy',
-        'Lost',
-        'Agents of Shield'
-    ];
-
-    $html = "<ul>";
-    foreach ($series as $serie){
-        $html .= "<li>$serie</li>";
-    }
-    $html .= "</ul>";
-
-    return $html;
-});
+Route::get('/series', 'SeriesController@index');
+Route::get('/series/criar', 'SeriesController@create');
